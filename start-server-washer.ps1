c25c1bcb58514310d0e0e2d6776dd8153efa59ba
andersla <anders@ormbunkar.se>
2019-12-14 09:54:35 +0100

    remove output redirect since it is not working from powershell

--- start-server-washer.ps1
+++ start-server-washer.ps1
@@ -1,11 +1,8 @@
 #
 # Startup script for webserver (can be installed and started as windows service)
 #
-$LOGFILE="C:\pharmbio\labrobots-restserver\server-washer.log"
-echo "Start new log" > $LOGFILE
-
-echo "Activate venv" >> $LOGFILE
+echo "Activate venv"
 C:\pharmbio\labrobots-restserver\venv\Scripts\Activate.ps1
 
 echo "Start webserver" >> "C:\pharmbio\labrobots-restserver\server-washer.log"
-python.exe 'C:\pharmbio\labrobots-restserver\server-washer.py' 2>&1 >> $LOGFILE
+python.exe C:\pharmbio\labrobots-restserver\server-washer.py
