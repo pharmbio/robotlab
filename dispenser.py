cacacd04494fd08f26066b12fe78e91a6d3483dc
andersla <anders.larsson@icm.uu.se>
2020-02-25 22:32:31 +0100

    change dispenser to new CLI version

--- dispenser.py
+++ dispenser.py
@@ -6,17 +6,27 @@ import sys
 
 from connexion import NoContent
 
+LHC_CALLER_CLI_PATH = "C:\Program Files (x86)\BioTek\Liquid Handling Control 2.22\LHC_CallerCLI.exe"
+BIOTEK_PRODUCT = "MultiFloFX"
+COM_PORT = "USB MultiFloFX sn:19041612"
 
-def execute_protocol(protocol_name):
 
-  logging.info("Inside execute_protocol, protocol_name=" + protocol_name)
+def execute_protocol(protocol_file):
+
+  logging.info("Inside execute_protocol, protocol_name=" + protocol_file)
 
   #
   # Execute on server an then return response
   #
-  proc_out = subprocess.Popen(["C:\Program Files (x86)\BioTek\Liquid Handling Control 2.22\LHC_CallerCLI.exe", "LHC_SetProductName", "Dispenser"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
+  proc_out = subprocess.Popen([LHC_CALLER_CLI_PATH,
+                               BIOTEK_PRODUCT,
+                               COM_PORT,
+                               "LHC_RunProtocol",
+                               protocol_file
+                               ],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT
+                              )
   stdout,stderr = proc_out.communicate()
 
   response = stdout
@@ -30,12 +40,17 @@ def status():
   #
   # Execute on server an then return response
   #
-  stdout = "Bla bla OK"
+  proc_out = subprocess.Popen([LHC_CALLER_CLI_PATH,
+                               BIOTEK_PRODUCT,
+                               COM_PORT,
+                               "LHC_GetProtocolStatus"
+                               ],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT
+                              )
+  stdout,stderr = proc_out.communicate()
 
-  #
-  # Maybe parse stdout
-  #
-  response = "OK"
+  response = stdout
 
   return response
 
@@ -47,7 +62,7 @@ def is_ready():
   #
   # Execute on server an then return response
   #
-  stdout = "Ready"
+  stdout = "???"
 
   #
   # Maybe parse stdout
@@ -60,7 +75,7 @@ def is_ready():
 
 if __name__ == '__main__':
     # Testrun
-    retval = is_ready()
+    retval = status()
     print(str(retval))
 
 
