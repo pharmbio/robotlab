998f7685b07f2eff3bc7ee36cf0862afdb1fdf8f
Dan Rosén <danr42@gmail.com>
2021-03-25 22:43:38 +0100

    bfs

--- controller.py
+++ controller.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field, replace
+from dataclasses import dataclass, field, replace, astuple
 from utils import dotdict
 from typing import Dict, Any, Tuple, Literal, NewType, Iterator
 import datetime
+
+from utils import show
+import snoop # type: ignore
+snoop.install(pformat=show)
+pp: Any
+
 
 JobId = NewType('JobId', str)
 
@@ -18,18 +24,25 @@ class Plate:
     loc: str
     lid_loc: str = 'self'
     target_loc: None | str = None
-    queue: list[ProtocolStep] = field(default_factory=list)
+    queue: list[ProtocolStep] = field(default_factory=list, repr=False)
     waiting_for: None | JobId | datetime.datetime | UnresolvedTime = None
-    meta: Any = None
+    meta: Any = field(default=None, repr=False)
+
+    def top(self) -> ProtocolStep:
+        return self.queue[0]
+
+    def pop(self) -> Plate:
+        return replace(self, queue=self.queue[1:])
 
 H = [21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
 I = [i+1 for i in range(42)]
 Out = [18] # [ i+1 for i in range(18) ] # todo: measure out_hotel_dist
+Out = [ i+1 for i in range(18) ] # todo: measure out_hotel_dist
 
-if 1:
+if 0:
     # small test version
     H = [21, 19, 17, 15, 13]
-    I = [1, 2, 3]
+    I = [1, 2, 3, 4, 5]
     Out = [18]
 
 h21 = 'h21'
@@ -45,6 +58,8 @@ class World:
     plates: dict[str, Plate]
 
     def __getattr__(self, loc: str) -> str:
+        if loc in ('shape', 'dtype'):
+            raise AttributeError
         for p in self.plates.values():
             if loc == p.loc:
                 return p.id
@@ -57,9 +72,11 @@ class World:
     # def __getitem__(self, loc: str) -> str:
     __getitem__ = __getattr__
 
+    def update(self, p: Plate) -> World:
+        return replace(self, plates={**self.plates, p.id: p})
+
     def success(self, p: Plate, cmds: list[run]=[]) -> Success:
-        w = replace(self, plates={**self.plates, p.id: p})
-        return Success(w=w, cmds=cmds)
+        return Success(w=self.update(p), cmds=cmds)
 
 @dataclass(frozen=True)
 class run:
@@ -169,7 +186,7 @@ class RT_incu(ProtocolStep):
                 if w[r_loc] == 'free':
                     return w.success(
                         replace(p, loc=r_loc, waiting_for=UnresolvedTime(self.time)),
-                        [run('robot', 'generated/{r_loc}_put')]
+                        [run('robot', f'generated/{r_loc}_put')]
                     )
         return None
 
@@ -181,7 +198,7 @@ class to_output_hotel(ProtocolStep):
                 if w[out_loc] == 'free':
                     return w.success(
                         replace(p, loc=out_loc),
-                        [run('robot', 'generated/{out_loc}_put')]
+                        [run('robot', f'generated/{out_loc}_put')]
                     )
         return None
 
@@ -218,30 +235,32 @@ def moves(p: Plate, w: World) -> Iterator[Success]:
         assert p.waiting_for is None
         yield w.success(
             replace(p, loc=h21),
-            [run('robot', 'generated/{r_loc}_get')]
+            [run('robot', f'generated/{p.loc}_get')]
         )
 
-    # h## to h21
-    if p.loc in h_locs and w.h21 == 'free':
-        yield w.success(
-            replace(p, loc=h21),
-            [run('robot', 'generated/{h_loc}_get')]
-        )
+    if 1:
+        # h## to h21
+        if p.loc in h_locs and w.h21 == 'free':
+            yield w.success(
+                replace(p, loc=h21),
+                [run('robot', f'generated/{p.loc}_get')]
+            )
 
-    # h21 to h##
-    if p.loc == h21:
-        for h_loc in h_locs:
-            if w[h_loc] == 'free':
-                yield w.success(
-                    replace(p, loc=h_loc),
-                    [run('robot', 'generated/{h_loc}_put')]
-                )
+        # h21 to h##
+        if p.loc == h21:
+            for h_loc in h_locs:
+                if w[h_loc] == 'free':
+                    yield w.success(
+                        replace(p, loc=h_loc),
+                        [run('robot', f'generated/{h_loc}_put')]
+                    )
+                    break
 
     # lid: move lid on h## to self
     if p.loc == h21 and p.lid_loc in lid_locs:
         yield w.success(
             replace(p, lid_loc='self'),
-            [run('robot', 'generated/lid_{p.lid_loc}_get')],
+            [run('robot', f'generated/lid_{p.lid_loc}_get')],
         )
 
     # delid: move lid on self to h##
@@ -250,8 +269,9 @@ def moves(p: Plate, w: World) -> Iterator[Success]:
             if w[lid_loc] == 'free':
                 yield w.success(
                     replace(p, lid_loc=lid_loc),
-                    [run('robot', 'generated/lid_{lid_loc}_put')],
+                    [run('robot', f'generated/lid_{lid_loc}_put')],
                 )
+                break
 
 # Cell Painting Workflow
 protocol: list[ProtocolStep] = [
@@ -286,25 +306,89 @@ protocol: list[ProtocolStep] = [
 
 from collections import deque
 
-def bfs(w0, moves, max_fuel = 10**5):
-    q = deque([(w0, [])])
+def bfs(w0: World, moves, max_fuel = 10_000) -> Tuple[World, list[run]] | None:
+    q: deque[Tuple[World, list[run]]] = deque([(w0, [])])
     visited = set()
     fuel = max_fuel
     while q and fuel > 0:
         fuel -= 1
         w, cmds = q.popleft()
-        if w in visited:
+        rw = repr(w)
+        if rw in visited:
             continue
-        visited.add(w)
+        visited.add(rw)
+        # pp(w.plates, world_locations(w))
         for p in w.plates.values():
             if p.waiting_for is not None:
                 continue
             if not p.queue:
                 continue
-            if res := p.queue[0].step(p, w):
+            if res := p.top().step(p.pop(), w):
+                # pp(p, p.top())
+                pp(max_fuel - fuel)
                 return (res.w, cmds + res.cmds)
-            for m in moves:
-                if res := m(p, w):
+            for res in moves(p, w):
+                if res:
                     q.append((res.w, cmds + res.cmds))
+    return None
+
+p0: list[Plate] = [
+    Plate('Ada', incu_locs[0], queue=protocol),
+    Plate('Bob', incu_locs[1], queue=protocol),
+    Plate('Cal', incu_locs[2], queue=protocol),
+    Plate('Deb', incu_locs[3], queue=protocol),
+    Plate('Eve', incu_locs[4], queue=protocol),
+    Plate('AII', incu_locs[5], queue=protocol),
+    Plate('BII', incu_locs[6], queue=protocol),
+    Plate('CII', incu_locs[7], queue=protocol),
+    Plate('DII', incu_locs[8], queue=protocol),
+    Plate('EII', incu_locs[9], queue=protocol),
+    Plate('Ad2', incu_locs[10], queue=protocol),
+    Plate('Bo2', incu_locs[11], queue=protocol),
+    Plate('Ca2', incu_locs[12], queue=protocol),
+    Plate('De2', incu_locs[13], queue=protocol),
+    Plate('Ev2', incu_locs[14], queue=protocol),
+    Plate('AI2', incu_locs[15], queue=protocol),
+    Plate('BI2', incu_locs[16], queue=protocol),
+    Plate('CI2', incu_locs[17], queue=protocol),
+    Plate('DI2', incu_locs[18], queue=protocol),
+    Plate('EI2', incu_locs[19], queue=protocol),
+]
+
+w0 = World(dict({p.id: p for p in p0}))
+
+w = w0
+
+all_cmds = []
+
+while 1:
+    # pp('running bfs...')
+    # for p in w.plates.values():
+    #     pp(p, len(p.queue), p.queue[:1])
+    res = bfs(w, moves)
+    if not res:
+        break
+    w, cmds = res
+    all_cmds += cmds
+    # pp('res:', w.plates, cmds)
+
+    for p in w.plates.values():
+        if p.target_loc == p.loc:
+            w = w.update(replace(p, target_loc=None))
+
+    for p in w.plates.values():
+        if p.waiting_for:
+            w = w.update(replace(p, waiting_for=None))
+
+print('done?')
+pp(all_cmds)
+pp({
+    p.id: (*astuple(p)[:4], len(p.queue))
+    for p in w.plates.values()
+})
+pp(w.plates)
+
+
+
 
 
--- scriptparser.py
+++ scriptparser.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from dataclasses import dataclass, field, replace, astuple
 from typing import Any
 
-
 from utils import show
 import snoop # type: ignore
 snoop.install(pformat=show)
