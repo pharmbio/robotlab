21218f58c19a748d20580aa5ed9742a3991f6b46
andersla <anders@ormbunkar.se>
2021-04-06 08:33:55 +0000

    First working STX incubator flask server

--- IncubatorSTX.py
+++ IncubatorSTX.py
@@ -2,12 +2,40 @@
 import logging
 import socket
 import time
+import threading
 #import os.path
-#import traceback
-#from flask import Flask, json
-#from flask import jsonify
+import traceback
+from flask import Flask, json
+from flask import jsonify
 
-ERROR_CODES_MOVE = """
+RETCODE_STX2RESET_OK = ""
+RETCODE_DESCR_STX2RESET = """
+    Return values: CR+LF.
+"""
+
+RETCODE_STX2ACTIVATE_OK = "1"
+RETCODE_DESCR_STX2ACTIVATE = """
+    Return values: the reply consists of one or two characters (separated by
+    semicolon) and CR+LF.
+    The first character is the device initialisation state:
+    1 - Communication is opened and device is initialised.
+    -1 - Error of opening Serial Port.
+    -2 - Error of opening Serial Port (serial Port is already opened).
+    -3 - No communication.
+    -4 - Communication Error.
+    -5 - System Error (System Error Flag is true).
+    -6 - User Door is opened (or cannot read User Door status).
+    -7 – User Door is unlocked or cannot lock a User Door (available if
+    door lock option present).
+    The second character depends on Barcode Reader presence and shows it
+    initialisation state :
+    1 - BCR Serial Port is successfully opened.
+    -1 - Error of opening BCR port.
+    -2 - Wrong value of BCR port.
+"""
+
+RETCODE_STX2SERVICEMOVEPLATE_OK = "1"
+RETCODE_DESCR_STX2SERVICEMOVEPLATE = """
     Returning values because of parameters error:
     -1 – Previous long operation is not finished.
     -2 - One of input parameters is not a valid integer value.
@@ -30,7 +58,7 @@ ERROR_CODES_MOVE = """
     -ID;8 – Device Status Error
 """
 
-class Incubator:
+class IncubatorSTX:
 
     def __init__(self, id, host, port):
         logging.debug("Inside init")
@@ -38,6 +66,28 @@ class Incubator:
         self.host = host
         self.port = port
 
+        # thread vars
+        self._synchronization_lock = threading.Lock()
+        self._thread_STX = None
+        self._last_STX_response = None
+
+    def _create_ok_response(self, method):
+        response = {"status": "OK",
+                    "value": "",
+                    "method": method,
+                    "details": ""}
+        logging.debug("response:" + str(response))
+        return response
+
+    def _create_error_response(self, method, value, description):
+        logging.debug("value:" + str(value))
+        logging.debug("description:" + str(description))
+        response = {"status": "ERROR",
+                    "value": str(value),
+                    "method": method,
+                    "details": description}
+        logging.debug("response:" + str(response))
+        return response
 
     def _movePlateFromCassetteToShovel(self, nCassette, nLevel): 
         id = self.id
@@ -57,7 +107,7 @@ class Incubator:
     
     def _movePlateFromTransferStationToCassette(self, nCassette, nLevel):
         id = self.id
-        self._stx2ServiceMovePlate(id,1,0,0,1,1,id,2,nCassette,nLevel,1,1) 
+        self._stx2ServiceMovePlate(id,1,0,0,1,1,id,2,nCassette,nLevel,1,1)
     
     def _movePlateFromCassetteToTransferStation(self, nCassette, nLevel):
         id = self.id
@@ -84,54 +134,92 @@ class Incubator:
                                     f"{trgID},{trgPos},{trgSlot},{trgLevel},{transTrgSlot},{trgPlType})\r"
         )
 
-        response = self._sendCmd(cmd)
-        if response != "1":
-            logging.error("Error codes: \n" + ERROR_CODES_MOVE)
-            raise Exception("_stx2ServiceMovePlate response error: " + response)
+        retval = self._sendCmd(cmd)
+        if retval == RETCODE_STX2SERVICEMOVEPLATE_OK:
+            response = self._create_ok_response("_stx2ServiceMovePlate")
+        else:
+            response = self._create_error_response("_stx2ServiceMovePlate", retval, RETCODE_DESCR_STX2SERVICEMOVEPLATE)
+
+        self._last_STX_response = response
 
 
     def _stx2GetSysStatus(self):
         logging.debug("Inside _stx2GetSysStatus")
         cmd = "STX2GetSysStatus(" + self.id + ")\r"
-        response = self._sendCmd(cmd)
+        retval = self._sendCmd(cmd)
 
     def _stx2Inventory(self):
         # Inventory file will be saved on STX-server in directory of jSTXDriver.jar
         logging.debug("Inside _stx2Inventory")
         cmd = "STX2Inventory(" + self.id + ",,0,0)\r"
-        response = self._sendCmd(cmd)
+        retval = self._sendCmd(cmd)
 
     def _stx2Reset(self):
         logging.debug("Inside _stx2Reset")
         cmd = "STX2Reset(" + self.id + ")\r"
-        response = self._sendCmd(cmd)
+        retval = self._sendCmd(cmd)
+        if retval == RETCODE_STX2RESET_OK:
+            response = self._create_ok_response("_stx2Reset")
+        else:
+            response = self._create_error_response("_stx2Reset", retval, RETCODE_DESCR_STX2RESET)
+
+        return response
 
     def _stx2Activate(self):
         logging.debug("Inside _stx2Activate")
         cmd = "STX2Activate(" + self.id + ")\r"
-        response = self._sendCmd(cmd)
-        if response != "1":
-            raise Exception("_stx2Activate response error: " + response)
+        retval = self._sendCmd(cmd)
+        if retval == RETCODE_STX2ACTIVATE_OK:
+            response = self._create_ok_response("_stx2Activate")
+        else:
+            response = self._create_error_response("_stx2Activate", retval, RETCODE_DESCR_STX2ACTIVATE)
+
+        return response
 
     def _stx2IsOperationRunning(self):
         logging.debug("Inside _stx2IsOperationRunning")
         cmd = "STX2IsOperationRunning(" + self.id + ")\r"
-        response = self._sendCmd(cmd)
-        if response == "-1":
-            raise Exception("_stx2IsOperationRunning response error: " + response)
-        return response
+        retval = self._sendCmd(cmd)
+
     
     def _stx2ReadActualClimate(self):
         logging.debug("Inside _stx2ReadActualClimate")
         cmd = "STX2ReadActualClimate(" + self.id + ")\r"
-        response = self._sendCmd(cmd)
+        retval = self._sendCmd(cmd)
+        return retval
 
-        return response
+    def resetAndActivate(self):
+        logging.debug("Inside resetAndActivate")
 
-    def resetAndAcrivate(self):
-        logging.debug("Inside resetAndAcrivate")
-        
-        
+        try:
+            # Dont worry if stx2Reset failed or not
+            unused_response = self._stx2Reset()
+            response = self._stx2Activate()
+
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            logging.error(e)
+            response = [{"status": "ERROR",
+                        "value": "",
+                        "details": "See log for traceback"}]
+            
+        finally:
+            logging.debug('Done finally')
+            return jsonify(response)
+
+    def is_ready(self):
+        logging.debug("Inside is_ready")
+        is_ready = self._is_STX_ready()
+        response = {"status": "OK",
+                    "value": is_ready,
+                    "details": ""}
+        return jsonify(response)
+
+    def _is_STX_ready(self):
+        if self._thread_STX is None or self._thread_STX.is_alive() == False:
+            return True
+        else:
+            return False
 
 
     def getClimate(self):
@@ -150,28 +238,95 @@ class Incubator:
 
         return climate
 
+
     def outputPlate(self, nPos):
         logging.debug("Inside outputPlate, nPos= " + str(nPos))
 
+
         # position 0-21  is cassette 1 level 1-22
         # position 22-43 is cassette 2 level 1-22
-
         nCassette = 1 + int(nPos / 22)
         nLevel = 1 + (nPos % 22)
 
-        self._movePlateFromCassetteToTransferStation(nCassette, nLevel)
+        # Synchronize the check_if_ready and execution of thread to avoid race between check and execution
+        self._synchronization_lock.acquire()
+        try:
+            if self._is_STX_ready():
+
+                self._thread_STX = threading.Thread(target=self._movePlateFromCassetteToTransferStation, args=([nCassette, nLevel]))
+                self._thread_STX.start()
+                response = {"status": "OK",
+                        "value": "",
+                        "details": "Executed protocol in background thread"}
+
+            else:
+
+                # send warning:
+                response = {"status": "WARNING",
+                            "value": "",
+                            "details": "Incubator is busy - will not run command"}
+                logging.warning(response)
+
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            logging.error(e)
+            response = [{"status": "ERROR",
+                        "value": "",
+                        "details": "See log for traceback"}]
+            
+        finally:
+            logging.debug('Done finally outputPlate')
+            self._synchronization_lock.release()
+            return jsonify(response)
 
 
     def inputPlate(self, nPos):
         logging.debug("Inside inputPlate, nPos= " + str(nPos))
 
+
         # position 0-21  is cassette 1 level 1-22
         # position 22-43 is cassette 2 level 1-22
-
         nCassette = 1 + int(nPos / 22)
         nLevel = 1 + (nPos % 22)
 
-        self._movePlateFromTransferStationToCassette(nCassette, nLevel)
+        # Synchronize the check_if_ready and execution of thread to avoid race between check and execution
+        self._synchronization_lock.acquire()
+        try:
+            if self._is_STX_ready():
+
+                self._thread_STX = threading.Thread(target=self._movePlateFromTransferStationToCassette, args=([nCassette, nLevel]))
+                self._thread_STX.start()
+                response = {"status": "OK",
+                        "value": "",
+                        "details": "Executed protocol in background thread"}
+
+            else:
+
+                # send warning:
+                response = {"status": "WARNING",
+                            "value": "",
+                            "details": "Incubator is busy - will not run command"}
+                logging.warning(response)
+
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            logging.error(e)
+            response = [{"status": "ERROR",
+                        "value": "",
+                        "details": "See log for traceback"}]
+            
+        finally:
+            logging.debug('Done finally inputPlate')
+            self._synchronization_lock.release()
+            return jsonify(response)
+
+    def get_last_STX_response(self):
+        logging.debug("Inside get_last_STX_response")
+        response = {"status": "OK",
+                    "value": self._last_STX_response_response,
+                    "details": ""}
+        return jsonify(response)
+
 
 
     def _sendCmd(self, cmd):
@@ -198,6 +353,8 @@ class Incubator:
 
 
 if __name__ == '__main__':
+
+
     #
     # Configure logging
     #
@@ -206,12 +363,15 @@ if __name__ == '__main__':
                         level=logging.DEBUG)
 
     rootLogger = logging.getLogger()
-    
-    incu = Incubator("STX", "130.238.44.60", 3333)
 
-    incu.stx2Reset()
-    incu.stx2Activate()
-    incu.getClimate()
+    server = Flask(__name__)
+    
+    incu = IncubatorSTX("STX", "130.238.44.60", 3333)
+
+    incu.resetAndActivate()
+    #incu.stx2Reset()
+    #incu.stx2Activate()
+    #incu.getClimate()
     #incu.movePlateFromCassetteToShovel(1,3)
     #incu.movePlateFromTransferStationToShovel()
     #incu.movePlateFromShovelToCassette(2,2)
@@ -231,3 +391,4 @@ if __name__ == '__main__':
     #incu.inputPlate(20)
     incu.inputPlate(22)
     #incu.inputPlate(18)
+
---
+++ server-incubatorSTX.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import logging
+from flask import Flask
+from IncubatorSTX import IncubatorSTX
+
+# Constants
+PORT = 5003
+
+if __name__ == '__main__':
+    # Init logging
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("Before start server")
+
+    # Create Incubator object
+    incubator = IncubatorSTX("STX", "130.238.44.60", 3333)
+
+    # Create webserver and map rest-api to runner methods
+    server = Flask(__name__)
+    server.add_url_rule('/input_plate/<int:nPos>', 'inputPlate', incubator.inputPlate)
+    server.add_url_rule('/output_plate/<int:nPos>', 'outputPlate', incubator.inputPlate)
+    server.add_url_rule('/is_ready', 'is_ready', incubator.is_ready)
+    server.add_url_rule('/resetAndActivate', 'resetAndActivate', incubator.resetAndActivate)
+    server.add_url_rule('/last_STX_response', 'last_STX_response', incubator.get_last_STX_response)
+    server.run(host='0.0.0.0', port=PORT)
+
+    #
+    # Example rest-calls
+    #
+    # http://localhost:5003/is_ready
+    # http://localhost:5003/resetAndActivate
+    # 
+    #
