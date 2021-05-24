83f49419e823847d1a26dd135760f3ff8c592bdf
Dan Rosén <danr42@gmail.com>
2021-05-24 08:13:46 +0200

    Tidy up utils a little

--- utils.py
+++ utils.py
@@ -77,6 +77,7 @@ def show(x: Any, show_key: Any=show_key, width: int=80) -> str:
                     oneline += out
                     if len(oneline) > 2 * width:
                         # oops lengths are totally wrong because of escape codes
+                        # idea: use textwrap.fill if all values are primitive
                         oneline = None
                         break
                 if oneline is None:
@@ -101,9 +102,9 @@ def show(x: Any, show_key: Any=show_key, width: int=80) -> str:
                 if isinstance(x, str):
                     yield dent + pre + color.green(lines[0]) + post
                 elif isinstance(x, (bool, type(None))):
-                    yield dent + pre + color.lightred(lines[0]) + post
-                elif isinstance(x, (int, float)):
                     yield dent + pre + color.purple(lines[0]) + post
+                elif isinstance(x, (int, float)):
+                    yield dent + pre + color.lightred(lines[0]) + post
                 else:
                     yield dent + pre + lines[0] + post
             else:
@@ -116,62 +117,8 @@ def show(x: Any, show_key: Any=show_key, width: int=80) -> str:
 
     return '\n'.join(go('', '', x, ''))
 
-A = TypeVar('A')
+_A = TypeVar('_A')
 
-def pr(x: A) -> A:
+def pr(x: _A) -> _A:
     print(show(x))
     return x
-
-class Expand():
-    '''
-    A copy of Haskell's enum literals which look like [1..10] and [1,3..10].
-
-    >>> expand[1, ..., 5]
-    [1, 2, 3, 4, 5]
-    >>> expand[5, ..., 1]
-    [5, 4, 3, 2, 1]
-    >>> expand[1, 3, ..., 7]
-    [1, 3, 5, 7]
-
-    Support for string prefixes, then it returns list of strings:
-    >>> print(*expand.h[1, ..., 5])
-    h1 h2 h3 h4 h5
-    >>> print(*expand.x[8, 6, ..., 0])
-    x8 x6 x4 x2 x0
-
-    Implemented using range so unfortunately it overshoots:
-    >>> expand[1, 3, ..., 8]
-    [1, 3, 5, 7, 9]
-    >>> expand[18, 13, ..., 0]
-    [18, 13, 8, 3, -2]
-    '''
-    def __getitem__(self, args: Tuple[int | Any, ...]) -> list[int]:
-        out = None
-        if len(args) == 3:
-            start, ellipsis, stop = args
-            if ellipsis == ...:
-                d = 1 if start < stop else -1
-                out = range(start, stop+d, d)
-        elif len(args) == 4:
-            start, next, ellipsis, stop = args
-            if ellipsis == ...:
-                d = next - start
-                out = range(start, stop+d, d)
-        if out is None:
-            raise ValueError(f'{args} not of right shape')
-        return list(out)
-
-    def __getattr__(self, prefix: str) -> ExpandPrefixed:
-        if not prefix.startswith('_') and prefix not in self.__dict__:
-            if self.prefix is not None:
-                raise ValueError(f'there is already a prefix {self.prefix!r}')
-        return ExpandPrefixed(prefix)
-
-class ExpandPrefixed():
-    def __init__(self, prefix: str):
-        self.prefix = prefix
-
-    def __getitem__(self, args: Tuple[int | Any, ...]) -> list[str]:
-        return [self.prefix + str(i) for i in Expand()[args]]
-
-expand = Expand()
