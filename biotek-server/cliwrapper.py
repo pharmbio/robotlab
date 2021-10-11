9833c8e078a5f023fae24d930b92fd507a3972c0
Dan Rosén <danr42@gmail.com>
2021-10-11 11:48:12 +0200

    Make biotek-server support REPLs

--- biotek-server/cliwrapper.py
+++ biotek-server/cliwrapper.py
@@ -4,54 +4,129 @@ import os.path
 import os
 import json
 from flask import Flask, jsonify
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
+from queue import Queue
+from typing import Callable, Any
+import threading
+import time
 
 LHC_CALLER_CLI_PATH = "C:\\Program Files (x86)\\BioTek\\Liquid Handling Control 2.22\\LHC_CallerCLI.exe"
-PROTOCOLS_ROOT = "C:\\ProgramData\\BioTek\\Liquid Handling Control 2.22\\Protocols"
+PROTOCOLS_ROOT = "C:\\ProgramData\\BioTek\\Liquid Handling Control 2.22\\Protocols\\"
 PORT = int(os.environ.get('PORT', 5050))
 HOST = os.environ.get('HOST', '10.10.0.56')
 
-def try_json(s):
-    try:
-        cleaned = s.strip().replace("\\","\\\\").replace("\r", "\\r").replace("\n","\\n")
-        return json.loads(cleaned)
-    except:
-        return s
+def spawn(f: Callable[[], None]) -> None:
+    threading.Thread(target=f, daemon=True).start()
 
-def main():
-    app = Flask(__name__)
-    @app.route('/<machine>/<sub_cmd>')
-    @app.route('/<machine>/<sub_cmd>/<path:path_arg>')
-    def execute(machine, sub_cmd, path_arg=None):
-        if machine == 'wash':
-            BIOTEK_PRODUCT = "405 TS/LS"
-            COM_PORT = "USB 405 TS/LS sn:191107F"
-            ARGS = [LHC_CALLER_CLI_PATH, BIOTEK_PRODUCT, COM_PORT]
-        elif machine == 'disp':
-            BIOTEK_PRODUCT = "MultiFloFX"
-            COM_PORT = "USB MultiFloFX sn:19041612"
-            ARGS = [LHC_CALLER_CLI_PATH, BIOTEK_PRODUCT, COM_PORT]
-        elif machine == 'echo':
-            ARGS = ['echo']
-        elif machine == 'help':
-            ARGS = ['help.exe']
+def machine(name: str, args: list[str]):
+
+    input_queue: Queue[tuple[str, str, Queue[Any]]] = Queue()
+    is_ready: bool = False
+
+    @spawn
+    def handler():
+        nonlocal is_ready
+        with Popen(
+            args,
+            stdin=PIPE,
+            stdout=PIPE,
+            # stderr=STDOUT,
+            stderr=PIPE,
+            # bufsize=1,  # line buffered
+            # universal_newlines=True,
+            encoding='utf-8',
+            # errors='replace',
+            # capture_output=True,
+        ) as p:
+            stdin = p.stdin
+            stdout = p.stdout
+            assert stdin
+            assert stdout
+
+            def read_to_ready(t0: float):
+                lines: list[tuple[float, str]] = []
+                while True:
+                    exc = p.poll()
+                    if exc is not None:
+                        t = round(time.monotonic() - t0, 3)
+                        lines += [(t, f"exit code: {exc}")]
+                        return lines
+                    line = stdout.readline().strip()
+                    t = round(time.monotonic() - t0, 3)
+                    print(t, name, line)
+                    if line.startswith('ready'):
+                        return lines
+                    else:
+                        lines += [(t, line)]
+
+            lines = read_to_ready(time.monotonic())
+            print(f"{lines = }")
+            while True:
+                is_ready = True
+                cmd, arg, reply_queue = input_queue.get()
+                is_ready = False
+                t0 = time.monotonic()
+                stdin.write(cmd + ' ' + arg + '\n')
+                stdin.flush()
+                lines = read_to_ready(t0)
+                success = any(line.startswith('success') for _, line in lines)
+                reply_queue.put_nowait(dict(success=success, lines=lines))
+
+    def message(cmd: str, arg: str=""):
+        if is_ready:
+            reply_queue: Queue[Any] = Queue()
+            input_queue.put((cmd, arg, reply_queue))
+            return reply_queue.get()
         else:
-            raise ValueError(machine)
-        args = [*ARGS, sub_cmd]
-        if isinstance(path_arg, str):
-            path_arg = os.path.join(PROTOCOLS_ROOT, path_arg)
-            args += [path_arg]
-        print('sub_cmd:', sub_cmd)
-        print('path_arg:', path_arg)
-        print('args:', args)
-        proc = Popen(args, stdout=PIPE, stderr=PIPE, stdin=None)
-        out, err = proc.communicate()
-        out = try_json(out.decode(errors='replace'))
-        err = try_json(err.decode(errors='replace'))
-        return dict(out=out, err=err)
+            return dict(success=False, lines=[(0.0, str("not ready"))])
 
-    app.run(host=HOST, port=PORT)
+    return message
+
+def example_main():
+    while True:
+        print("ready")
+        line = input()
+        print("message", line)
+        if "error" in line:
+            print("error")
+        else:
+            print("success")
+
+def main(test: bool):
+    if test:
+        machines = {
+            'example': machine(
+                'example',
+                args=['python', __file__, "--example"]
+            ),
+        }
+    else:
+        machines = {
+            'example': machine(
+                'example',
+                args=['python', __file__, "--example"],
+            ),
+            'wash': machine(
+                'wash',
+                args=[LHC_CALLER_CLI_PATH, "405 TS/LS", "USB 405 TS/LS sn:191107F"],
+            ),
+            'disp': machine(
+                'disp',
+                args=[LHC_CALLER_CLI_PATH, "MultiFloFX", "USB MultiFloFX sn:19041612"],
+            ),
+        }
+
+    app = Flask(__name__)
+    @app.route('/<machine>/<cmd>')             # type: ignore
+    @app.route('/<machine>/<cmd>/<path:arg>')  # type: ignore
+    def execute(machine: str, cmd: str, arg: str=""):
+        return jsonify(machines[machine](cmd, arg))
+
+    app.run(host=HOST, port=PORT, threaded=True, processes=1)
 
 if __name__ == '__main__':
-    main()
+    if '--example' in sys.argv:
+        example_main()
+    else:
+        main('--test' in sys.argv)
 
