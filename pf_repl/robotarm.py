5810793d466b76b147e755bb8566b700c70eb35b
Dan Rosén <danr42@gmail.com>
2022-01-31 15:41:00 +0100

    Add flash.py

--- pf_repl/robotarm.py
+++ pf_repl/robotarm.py
@@ -5,89 +5,52 @@ from typing import *
 
 import socket
 from utils import Mutable
-from pathlib import Path
-from ftplib import FTP
-import io
-import time
 
 DEFAULT_HOST='10.10.0.98'
 
-def ftp_store(ftp: FTP, filename: str, data: bytes):
-    ftp.storbinary(f'STOR {filename}', io.BytesIO(data))
-
 @dataclass(frozen=True)
 class Robotarm:
-    # sock: socket.socket
-    host: str
+    sock: socket.socket
     data: Mutable[str] = Mutable.factory('')
 
     @staticmethod
-    def init(host: str=DEFAULT_HOST, port: int=23, password: str='Help') -> Robotarm:
-        # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # sock.connect((host, port))
-        arm = Robotarm(host)
-        print(next(arm.recv()))
-        arm.send(password)
-        print(next(arm.recv()))
-        arm.wait_for_ready()
-        return arm
+    def init(host: str=DEFAULT_HOST, port: int=10100) -> Robotarm:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        return Robotarm(sock)
 
     def recv(self):
-        yield ''
-
-    def flash(self):
-        self.send('stop -all')
-        # time.sleep(0.5)
-        self.wait_for_ready('stop')
-        self.send('unload -all')
-        # time.sleep(0.5)
-        self.wait_for_ready('unload')
-        project = Path('Tcp_cmd_server')
-        dir = f'/flash/projects/{project}'
-        self.execute(f'File.CreateDirectory("{dir}")')
-        self.wait_for_ready('mkdir')
-        with FTP(self.host) as ftp:
-            ftp.login()
-            for path in project.glob('*.gp*'):
-                print('ftp store', path.name)
-                ftp_store(ftp, f'{dir}/{path.name}', path.read_bytes())
-        for cmd in [
-            f'load {dir} -compile',
-            'execute StartMain()',
-        ]:
-            self.send(cmd)
-            self.wait_for_ready(cmd.split(' ')[0])
-
-    def quit(self):
-        self.send('quit')
-        for line in self.recv():
-            if line.startswith('Exiting console task'):
+        while True:
+            try:
+                b = self.sock.recv(4096)
+            except OSError as e:
+                print(e)
                 break
-        self.close()
+            self.data.value += b.decode(errors='replace').replace('\r\n', '\n').replace('\r', '\n')
+            lines = self.data.value.splitlines(keepends=True)
+            next_data = ''
+            full_lines: list[str] = []
+            for line in lines:
+                if '\n' in line:
+                    line = line.rstrip()
+                    if line:
+                        print('<<<', line)
+                        full_lines += [line]
+                else:
+                    if next_data:
+                        print('warning: multiple "lines" without newline ending:', lines)
+                    next_data += line
+            self.data.value = next_data
+            yield from full_lines
 
     def wait_for_ready(self, msg: str='ready'):
-        # time.sleep(0.1)
-        # self.log(msg)
-        # for line in self.recv():
-        #     if line.startswith(f'log {msg}') :
-        #         time.sleep(0.1)
-        #         return
         pass
 
     def send(self, msg: str):
         print('>>>', msg)
         msg += '\n'
-        # self.sock.sendall(msg.encode())
-        # time.sleep(0.1)
-
-    def execute(self, stmt: str):
-        self.send(f'execute {stmt}')
-
-    def log(self, msg: str):
-        assert '"' not in msg
-        self.execute(f'Console.WriteLine("log {msg}")')
+        self.sock.sendall(msg.encode())
 
     def close(self):
-        pass
-        # self.sock.close()
+        self.sock.close()
 
