eb043fa988e8d9a44b3c5958d926815e1324205b
Dan Rosén <danr42@gmail.com>
2023-09-07 11:14:53 +0200

    Try without wt

--- labrobots/run_labrobots.bat
+++ labrobots/run_labrobots.bat
@@ -5,4 +5,13 @@ echo :loop >> temp_loop.bat
 echo call labrobots.exe >> temp_loop.bat
 echo goto loop >> temp_loop.bat
 
-wt -p "Command Prompt" -d /robotlab/labrobots cmd.exe /K temp_loop.bat
+:: Try running wt to see if it exists
+wt /? >nul 2>nul
+if errorlevel 1 (
+    :: wt doesn't exist, use cmd.exe
+    :: this is for the GBG computer
+    start cmd.exe -d /robotlab/labrobots /K temp_loop.bat
+) else (
+    :: wt exists, use it
+    wt -p "Command Prompt" -d /robotlab/labrobots cmd.exe /K temp_loop.bat
+)
