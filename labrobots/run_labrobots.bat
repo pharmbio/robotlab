dcd574c8fb69123f18cfacd195182617715dc073
Dan Rosén <danr42@gmail.com>
2023-09-07 11:06:48 +0200

    Use windows terminal to run labrobots

--- labrobots/run_labrobots.bat
+++ labrobots/run_labrobots.bat
@@ -1,4 +1,8 @@
-cd /robotlab/labrobots
-:loop
-call labrobots.exe
-goto loop
+:: Hand over to windows terminal (wt) which does not block the process when selecting text
+
+echo cd /robotlab/labrobots > temp_loop.bat
+echo :loop >> temp_loop.bat
+echo call labrobots.exe >> temp_loop.bat
+echo goto loop >> temp_loop.bat
+
+wt -p "Command Prompt" -d /robotlab/labrobots cmd.exe /K temp_loop.bat
