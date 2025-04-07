23f5286cf93f32ccfcf45be43d1f64ba278bc01e
Dan Rosén <danr42@gmail.com>
2025-04-07 16:44:33 +0200

    Update labeler

--- labrobots/labrobots/labeler.py
+++ labrobots/labrobots/labeler.py
@@ -17,41 +17,35 @@ P = ParamSpec('P')
 R = TypeVar('R')
 
 @dataclass(frozen=True)
-class QueueEntry:
-    msg: str
-    log: Log
-    reply_queue: Queue[Any]
-
-    def reply(self, lines: list[dict[str, Any]]):
-        error = None
-        for line in lines:
-            if 'error' in line:
-                error = line
-        if error is not None:
-            self.reply_queue.put_nowait(dict(**error, lines=lines, success=False))
-        else:
-            self.reply_queue.put_nowait(dict(**lines[-1], lines=lines, success=True))
-
-@dataclass(frozen=True)
 class Labeler(Machine):
     args: List[str] = field(default_factory=list)
-    input_queue: 'Queue[QueueEntry]' = field(default_factory=Queue)
+    input_queue: 'Queue[str]' = field(default_factory=Queue)
 
     event_listeners_lock: Lock = field(default_factory=Lock)
     event_listeners: 'list[Queue[str]]' = field(default_factory=list)
+
+    stdout_forward: 'Queue[dict[str, Any]]' = field(default_factory=Queue)
+    process_dead_error: dict[str, Any] = field(default_factory=dict)
 
     def init(self):
         threading.Thread(target=self._handler, daemon=True).start()
 
     def _send(self, cmd: str, *args: str) -> dict[str, Any]:
         with self.exclusive():
-            reply_queue: Queue[Any] = Queue()
             msg = ' '.join([
                 part.replace(' ', '\\s').replace('\n', '\\s').encode('ascii', errors='replace').decode()
                 for part in [cmd, *args]
             ])
-            self.input_queue.put(QueueEntry(msg, self.log, reply_queue))
-            return reply_queue.get()
+            self.input_queue.put(msg)
+            lines: list[dict[str, Any]] = []
+            while True:
+                if self.process_dead_error:
+                    return dict(self.process_dead_error)
+                data = self.stdout_forward.get()
+                self.log(repr(data), **data)
+                if data.get('status') == 'ready' and len(lines) > 1:
+                    return dict(**lines[-1], lines=lines)
+                lines += [data]
 
     def _handler(self):
         with Popen(
@@ -69,65 +63,45 @@ class Labeler(Machine):
             assert stdin
             assert stdout
 
-            current_queue_entry: QueueEntry | None
-            t0 = time.monotonic()
+            log = Log.make('labeler')
 
-            labeler_log = Log.make('labeler')
-
-            def reader():
-                nonlocal current_queue_entry, t0
-                lines: List[Dict[str, Any]] = []
+            def writer():
                 while True:
-                    if current_queue_entry:
-                        log = current_queue_entry.log
-                        reply = current_queue_entry.reply
-                    else:
-                        log = labeler_log
-                        reply = labeler_log
-                    exc = p.poll()
-                    if exc is not None:
-                        t = round(time.monotonic() - t0, 3)
-                        log(t, f"exit code: {exc}")
-                        lines += [{'error': 'exit', 'exit_code': exc}]
-                        reply(lines)
-                        lines = []
-                    line = stdout.readline().rstrip()
-                    t = round(time.monotonic() - t0, 3)
-                    short_line = line
-                    if len(short_line) > 250:
-                        short_line = short_line[:250] + '... (truncated)'
-                    log(t, short_line)
-                    try:
-                        data = json.loads(line)
-                    except json.JSONDecodeError as e:
-                        data = {'error': 'JSONDecodeError', 'details': str(e), 'line': line}
-                        reply([*lines, data])
-                        lines = []
-                        continue
-                    except Exception as e:
-                        data = {'error': str(type(e)), 'details': str(e), 'line': line}
-                        reply([*lines, data])
-                        lines = []
-                        continue
-                    log(**data)
-                    if data.get('status') == 'ready':
-                        reply(lines)
-                        lines = []
-                    elif (event := data.get('event')):
-                        with self.event_listeners_lock:
-                            for listener in self.event_listeners:
-                                labeler_log(f'Notifying listener of event {event!r}')
-                                listener.put_nowait(event)
-                    else:
-                        lines += [data]
+                    msg = self.input_queue.get()
+                    if self.process_dead_error:
+                        return
+                    log(f'repl.write({msg!r})', msg=msg)
+                    stdin.write(msg + '\n')
+                    stdin.flush()
 
-            threading.Thread(target=reader, daemon=True).start()
+            threading.Thread(target=writer, daemon=True).start()
 
             while True:
-                queue_entry = self.input_queue.get()
-                t0 = time.monotonic()
-                stdin.write(queue_entry.msg + '\n')
-                stdin.flush()
+                exc = p.poll()
+                if exc is not None:
+                    log(f'exit code: {exc}')
+                    self.process_dead_error.update({'error': 'exit', 'exit_code': exc})
+                    self.stdout_forward.put_nowait(self.process_dead_error)
+                    return
+
+                line = stdout.readline().rstrip()
+                try:
+                    data = json.loads(line)
+                except json.JSONDecodeError as e:
+                    data = {'error': 'JSONDecodeError', 'details': str(e), 'line': line}
+                except Exception as e:
+                    data = {'error': str(type(e)), 'details': str(e), 'line': line}
+                log(f'repl.read().as_json() = {data!r}', line=line, **data)
+                if (event := data.get('event')):
+                    with self.event_listeners_lock:
+                        listeners = self.event_listeners
+                        self.event_listeners.clear()
+                    for listener in listeners:
+                        log(f'Notifying listener {listener} of event {event!r}')
+                        listener.put_nowait(event)
+                else:
+                    self.stdout_forward.put_nowait(data)
+
 
     @staticmethod
     def _rpc(fn: Callable[Concatenate['Labeler', P], R]) -> Callable[Concatenate['Labeler', P], R]:
@@ -150,7 +124,7 @@ class Labeler(Machine):
             self.event_listeners.append(listener)
         try:
             self.log('Listening for event')
-            event = listener.get(timeout=10.0)
+            event = listener.get(timeout=30.0)
             self.log(f'Received event {event!r}')
             if event == 'green_button':
                 return event
